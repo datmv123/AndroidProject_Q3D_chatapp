b1ab5742c01e6154415e4a4db18376122fde9049
datmv123 <datmv123@users.noreply.github.com>
2019-11-08 19:51:57 +0700

    first commit: setup project

---
+++ chatapp/app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                System.err.println(item.getItemId() + "-------------");
+                bottomNavigationView.setSelected(false);
+                return true;
+            }
+        });
+    }
+
+}
