99cba6f2eab08e9835caa7304ab2a9163e9583f6
datmv123 <datmv123@users.noreply.github.com>
2019-11-11 21:06:02 +0700

    update search vs message

--- chatapp/app/src/main/java/com/example/chatapp/MainActivity.java
+++ chatapp/app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.chatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -9,7 +12,16 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.chatapp.messages.MessageActivity;
+import com.example.chatapp.messages.model.UserInfo;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword( "admin@123.com",  "admin@123").addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(MainActivity.this, "Login sucessful", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(MainActivity.this, "Login fail", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
---
+++ chatapp/app/src/main/java/com/example/chatapp/messages/constant/DatabaseName.java
@@ -0,0 +1,7 @@
+package com.example.chatapp.messages.constant;
+
+public final class DatabaseName {
+    public static final String DB_USERS = "users";
+    public static final String DB_MESSAGE_DETAIL = "messages";
+    public static final String DB_MESSAGE_LIST = "messageList";
+}
